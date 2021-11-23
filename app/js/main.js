f02a1913b33a74be6da00fecb8c6944dab0a920c
Garmanika <aleks.orlov97@gmail.com>
2021-11-23 16:50:59 +0300

    23/11

--- app/js/main.js
+++ app/js/main.js
@@ -1,2 +1,8 @@
 $(function () {
+	$(".menu-btn").on("click", function() {
+		$(".header-bottom").fadeToggle();
+		$(".menu-btn").toggleClass("open-menu");
+		$(".header-wrapper").toggleClass("open-menu");
+		$("body").toggleClass("noscroll");
+});
 });
